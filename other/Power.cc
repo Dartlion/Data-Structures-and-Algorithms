21b285db7ad50341e1797933dfdfa3943cad6f99
dartlion <eping.hon@gmail.com>
2013-06-15 19:45:28 +0800

    other algorithm

---
+++ other/Power.cc
@@ -0,0 +1,32 @@
+#include "iostream"
+using namespace std;
+
+#define DEBUG
+/*calaculate x^idx*/
+int power(int x, int idx)
+{
+	if (0 == idx)
+	{
+		return 1;
+	}
+	if (1 == idx)
+	{
+		return x;
+	}
+	if (0 == idx%2)
+	{
+		return power(x*x, idx/2);
+	}
+	if (1 == idx%2)
+	{
+		return power(x*x, idx/2) * x;		
+	}
+}
+
+int main()
+{
+	cout<< power(2,0) <<endl;
+	cout<< power(2,1) <<endl;
+	cout<< power(2,10) <<endl;
+	cout<< power(2,11) <<endl;
+}
